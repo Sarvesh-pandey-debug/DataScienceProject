3af59f1e9aa144e751ef9df0242c9dc968433657
sarveshzversal <sarveshzversal@gmail.com>
2026-01-25 14:43:27 +0530

    data ingestion part completed with notebook and modular coding

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 ## E2E-DS project
+
+### workflow -- ML Pipeline
+
+1. Data ingestion 
+2. Data validation
+3. Data Transformation
+4. Model Trainer
+5. Model Evaluation
+
+
+
+### Workflows below these update must be update when wright code any of components above wright
+
+1. Update config.yaml
+2. Uodate Schema.yaml
+3. Update params.yamal
+4. Update the entity
+5. Update the configuration manager in src confi
+6. Update the components
+7. Update the pipeline 
+8. update the main.py
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
 logger.info("wellcome to datascience custome logger")
+
+STAGE_NAME = "Data ingestion stage"
+
+try:
+    logger.info(f"....... stage {STAGE_NAME}  started .......") 
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f"...... stage {STAGE_NAME}  completed ........\n\nx=======x")
+except Exception as e:
+    logger.exception(e)
+    raise e           
+        
