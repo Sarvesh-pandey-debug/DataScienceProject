ce02e699266f55275ae59fba3269c8ab205bf724
sarveshzversal <sarveshzversal@gmail.com>
2026-01-26 21:49:40 +0530

    Model training  part completed with notebook and modular coding

--- src/datascience/entity/config_entity.py
+++ src/datascience/entity/config_entity.py
@@ -25,4 +25,20 @@ class DataValidationConfig:
 @dataclass 
 class DataTransformationConfig:
     root_dir: Path
-    data_path: Path    
+    data_path: Path  
+    
+    
+    
+## cinfig for model trainer
+
+@dataclass
+class ModelTrainerConfig:
+    root_dir: Path
+    train_data_path : Path
+    test_data_path : Path
+    model_name: str
+    
+    alpha: float     ## these 3 parameter use for elasticnet regularization towards using elasticnet in this have 2 parameter 
+    l1_ratio: float
+    target_column: str
+          
